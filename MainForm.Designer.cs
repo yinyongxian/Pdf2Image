e36c2acdcd3f1709feff0e12833996545f9874fd
yinyongxian <yinyongxian@qq.com>
2023-10-26 01:22:43 +0800

    Add images

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -28,14 +28,13 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(MaintForm));
             labelFilePath = new Label();
             textBoxFilePath = new TextBox();
             buttonFilePath = new Button();
             buttonOutputFolder = new Button();
             textBoxOutputFolder = new TextBox();
             labelOutputFolder = new Label();
-            comboBox1 = new ComboBox();
-            labelImageType = new Label();
             checkBoxLongImage = new CheckBox();
             buttonOK = new Button();
             SuspendLayout();
@@ -94,30 +93,12 @@
             labelOutputFolder.TabIndex = 3;
             labelOutputFolder.Text = "Output Folder:";
             // 
-            // comboBox1
-            // 
-            comboBox1.Enabled = false;
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(110, 88);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(229, 25);
-            comboBox1.TabIndex = 6;
-            // 
-            // labelImageType
-            // 
-            labelImageType.AutoSize = true;
-            labelImageType.Location = new Point(12, 92);
-            labelImageType.Name = "labelImageType";
-            labelImageType.Size = new Size(80, 17);
-            labelImageType.TabIndex = 3;
-            labelImageType.Text = "Image Type:";
-            // 
             // checkBoxLongImage
             // 
             checkBoxLongImage.AutoSize = true;
             checkBoxLongImage.Checked = true;
             checkBoxLongImage.CheckState = CheckState.Checked;
-            checkBoxLongImage.Location = new Point(110, 128);
+            checkBoxLongImage.Location = new Point(110, 88);
             checkBoxLongImage.Name = "checkBoxLongImage";
             checkBoxLongImage.Size = new Size(97, 21);
             checkBoxLongImage.TabIndex = 7;
@@ -126,7 +107,8 @@
             // 
             // buttonOK
             // 
-            buttonOK.Location = new Point(697, 426);
+            buttonOK.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            buttonOK.Location = new Point(697, 87);
             buttonOK.Name = "buttonOK";
             buttonOK.Size = new Size(75, 23);
             buttonOK.TabIndex = 8;
@@ -136,21 +118,21 @@
             // 
             // MaintForm
             // 
+            AcceptButton = buttonOK;
             AutoScaleDimensions = new SizeF(7F, 17F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(784, 461);
+            ClientSize = new Size(784, 122);
             Controls.Add(buttonOK);
             Controls.Add(checkBoxLongImage);
-            Controls.Add(comboBox1);
             Controls.Add(buttonOutputFolder);
             Controls.Add(textBoxOutputFolder);
-            Controls.Add(labelImageType);
             Controls.Add(labelOutputFolder);
             Controls.Add(buttonFilePath);
             Controls.Add(textBoxFilePath);
             Controls.Add(labelFilePath);
+            Icon = (Icon)resources.GetObject("$this.Icon");
             Name = "MaintForm";
-            Text = "MainForm";
+            Text = "Pdf to Image";
             ResumeLayout(false);
             PerformLayout();
         }
@@ -163,8 +145,6 @@
         private Button buttonOutputFolder;
         private TextBox textBoxOutputFolder;
         private Label labelOutputFolder;
-        private ComboBox comboBox1;
-        private Label labelImageType;
         private CheckBox checkBox1;
         private Button buttonOK;
         private CheckBox checkBoxLongImage;
